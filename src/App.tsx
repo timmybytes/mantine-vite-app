e8097ea7186589e8a55c3cd8b249ce6d9bf19a44
Timothy Merritt <timmybytes@gmail.com>
2021-07-12 18:05:04 -0700

    chore: Add to basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,9 @@ export default function App() {
   // TODO: Add params to pass to the layout components
   return (
     <Layout>
-      {/* TODO: Move css to a separate file */}
-      <div
-        style={{
-          position: 'absolute',
-          width: '100%',
-          zIndex: 2,
-          maxWidth: '600px',
-        }}>
+      <div className='layout__content'>
         <h1>A new way to do things</h1>
-        <p style={{ fontSize: '1.25rem' }}>
+        <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
           expedita sit iusto atque aliquam sunt excepturi?
         </p>
@@ -25,21 +18,7 @@ export default function App() {
           Start Now
         </Button>
       </div>
-
-      <img
-        style={{
-          width: '100%',
-          height: 'auto',
-          maxWidth: '200px',
-          right: '25%',
-          top: '29%',
-          position: 'absolute',
-          zIndex: 1,
-          opacity: 0.8,
-        }}
-        src={bookIso}
-        alt='alt'
-      />
+      <img className='layout__cover' src={bookIso} alt='alt' />
     </Layout>
   );
 }
